280cb001c34e89502b2fdf8e48867fa417d3cdf8
m-takada <emerald832@gmail.com>
2018-04-20 09:37:39 +0900

    :+1:

---
+++ Assets/Scripts/UISwipeableCard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SwipeableView
+{
+    public class UISwipeableCard<TData, TContext> : MonoBehaviour where TContext : class
+	{
+        private TContext context;
+
+        public virtual void SetContext(TContext context)
+        {
+            this.context = context;
+        }
+
+        public virtual void SetVisible(bool visible)
+        {
+        }
+	}
+}
+
---
+++ Assets/Scripts/UISwipeableView.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SwipeableView
+{
+    public class UISwipeableView<TData, TContext> : MonoBehaviour where TContext : class
+	{
+        [SerializeField]
+        private GameObject cardPrefab;
+
+        [SerializeField]
+        private Transform cardRoot;
+
+
+        private TContext context;
+        private readonly List<UISwipeableCard<TData, TContext>> cards
+            = new List<UISwipeableCard<TData, TContext>>();
+
+
+
+        private UISwipeableCard<TData, TContext> CreateCard()
+        {
+            var cardObject = Object.Instantiate(cardPrefab, cardRoot);
+            cardPrefab.SetActive(true);
+            cardObject.transform.SetAsLastSibling();
+
+            var card = cardObject.GetComponent<UISwipeableCard<TData, TContext>>();
+            card.SetContext(context);
+            card.SetVisible(false);
+
+            return card;
+        }
+	}
+}
+
